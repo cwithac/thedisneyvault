a892b1d621a77c6bb158ad590e84b24d06a2334b
Cathleen Wright <cathleenmwright@gmail.com>
2017-04-15 14:26:00 -0400

    Session Create Route

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,6 +4,7 @@
 //---------------------------------
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users.js');
 
 //ROUTES
 //---------------------------------
@@ -11,6 +12,17 @@ var router = express.Router();
 //NEW SESSION LOGIN ROUTE
 router.get('/signin', function(req, res) {
   res.render('sessions/signin.ejs')
+});
+
+//NEW SESSION LOGIN CREATE ROUTE
+router.post('/', function(req, res) {
+  User.findOne({ username: req.body.username }, function(err, foundOneUser) {
+    if (req.body.password === foundOneUser.password) {
+      res.send('Signed In');
+    } else {
+      res.send('Incorrect Password');
+    }
+  });
 });
 
 //LISTENERS
