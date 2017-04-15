107fc83a4b2953cdc62a70c6f84b68afecce7bc6
Cathleen Wright <cathleenmwright@gmail.com>
2017-04-15 14:09:09 -0400

    Installed Mongoose

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var mongoose = require('mongoose');
 
 //MIDDLEWARE
 //---------------------------------
@@ -21,6 +22,13 @@ app.get('/', function(req, res){
 
 //LISTENERS
 //---------------------------------
+
+mongoose.connect('mongodb://localhost:27017/thedisneyvault');
+
+mongoose.connection.once('open', function() {
+  console.log('server.js is connected to mongo');
+});
+
 app.listen(3000, function() {
   console.log('server.js is listening to port ' + port);
 });
