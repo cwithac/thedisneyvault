0a5aa44aee913cc7327650125a796ba99646923e
Cathleen Wright <cathleenmwright@gmail.com>
2017-04-16 19:00:55 -0400

    Character Index and Add/New

--- controllers/characters.js
+++ controllers/characters.js
@@ -4,12 +4,34 @@
 //---------------------------------
 var express = require('express');
 var router = express.Router();
+var Character = require('../models/characters.js');
 
 //ROUTES
 //---------------------------------
 
+//CHARACTERS INDEX
+// router.get('/', function(req, res) {
+//   res.render('characters/index.ejs')
+// });
+
+//CHARARCTERS INDEX UPDATED
 router.get('/', function(req, res) {
-  res.render('characters/index.ejs')
+  Character.find({}, function(err, foundCharacters){
+    res.render('characters/index.ejs', {
+          characters: foundCharacters
+    });
+  });
+});
+
+//ADD CHARACTERS
+router.get('/add', function(req, res) {
+  res.render('characters/new.ejs');
+});
+
+router.post('/', function(req, res) {
+  Character.create(req.body, function(err, createdCharacter) {
+    res.redirect('/characters')
+  });
 });
 
 //LISTENERS
---
+++ models/characters.js
@@ -0,0 +1,25 @@
+//CHARACTERS MODEL
+
+//DEPENDENCIES
+//---------------------------------
+var mongoose = require('mongoose');
+var charSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    headshot: { type: String, required: true },
+    location: { type: String },
+    kind: Array,
+    quote: { type: String },
+    enemy: { type: String },
+    love: { type: String },
+    friends: Array,
+    additional: Array,
+    movie: [] //[FILM SCHEMA]
+});
+
+var Character = mongoose.model('Character', charSchema);
+
+
+//LISTENERS
+//---------------------------------
+
+module.exports = Character;
