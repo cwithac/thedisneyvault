bed14fbc9ad8086676ecdbf8896104543056d08c
Cathleen Wright <cathleenmwright@gmail.com>
2017-04-17 14:14:47 -0400

    Search only shows films with posters and movies only

--- public/js/app.js
+++ public/js/app.js
@@ -22,27 +22,34 @@ var findFilms = function() {
   var $searchedValue = $searchBox.val();
   console.log("Searched Input:" + $searchedValue);
   var $searchResults = $('#search-results');
-  $.ajax('https://www.omdbapi.com/?t=' + $searchedValue + '&r=json')
+  $.ajax('https://www.omdbapi.com/?s=' + $searchedValue + '&r=json')
 
   .done(function(filmsList) {
     console.log("JSON:");
     console.log(filmsList);
+    console.log(filmsList.Search);
     $searchResults.empty()
-      if (filmsList.Poster === "N/A") { filmsList.Poster = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"; };
-    var $poster = $('<img>').attr('src', filmsList.Poster)
-    $poster.attr('class', 'poster');
-    var $addButton = $('<button id=' + filmsList.imdbID + ' type="submit">Add Film</button>');
-    $addButton.on('click', addButtonClicked)
-    var $resultsBox = $('<div>')
-    $resultsBox.append($poster);
-    $resultsBox.append($addButton);
-    $searchResults.append($resultsBox)
 
+    for (var i = 0; i < filmsList.Search.length; i++) {
+      if ((filmsList.Search[i].Poster !== "N/A") && (filmsList.Search[i].Type === "movie")) {
+      var $poster = $('<img>').attr('src', filmsList.Search[i].Poster)
+      $poster.attr('class', 'poster');
+      var $addButton = $('<button id=' + filmsList.Search[i].imdbID + ' type="submit">Add Film</button>');
+      $addButton.on('click', addButtonClicked)
+      var $resultsBox = $('<div>')
+      $resultsBox.append($poster);
+      $resultsBox.append($addButton);
+      $searchResults.append($resultsBox)
+    } else {
+        if (filmsList.Search[i].Poster === "N/A") { filmsList.Search[i].Poster = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"; };
+      }
+    }
     $searchBox.val('');
     addButtonClicked();
   })
 };
 
 var addButtonClicked = function() {
-  console.log(this.id);
+  console.log("imdbID: " + this.id);
+
 };
