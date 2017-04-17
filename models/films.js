07de8c94a6e6f95f6a5bcfb669f3f29c2726d74b
Cathleen Wright <cathleenmwright@gmail.com>
2017-04-17 08:48:12 -0400

    Films Model Cleanup

--- models/films.js
+++ models/films.js
@@ -5,7 +5,7 @@
 var mongoose = require('mongoose');
 var filmSchema = mongoose.Schema({
     title: { type: String, required: true },
-    poster: { type: String, required: true },
+    poster: { type: String },
     year: { type: Number, min: 1900, max: 2020 },
     imdb: { type: String },
     characters: [] //[CHARACTERS SCHEMA]
