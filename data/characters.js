230c70f0d2e4ea8faaabece52786710b6c52e941
Cathleen Wright <cathleenmwright@gmail.com>
2017-04-20 09:57:16 -0400

    Heroku Data Files

---
+++ data/characters.js
@@ -0,0 +1,312 @@
+//LIST OF CHARACTER OBJECTS FOR HEROKU
+//NOT A DATA SEED
+//PRIMARY SOURCE: http://movies.disney.com/
+
+Snow White = {
+  Name: "Snow White",
+  Film: "Snow White and the Seven Dwarfs",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/character_princess_snowwhite_b6c31f4d.jpeg?region=0,0,563,300",
+  Setting: "Germany",
+  Type: "Hero",
+  Quote: "Just whistle while you work!",
+  Nemesis: "The Evil Queen",
+  Love: "The Prince",
+  Sidekicks: "The Seven Dwarfs"
+}
+
+Cinderella = {
+  Name: "Cinderella",
+  Film: "Cinderella",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/2b67814999ed3225d5e78590a4126a8b15625116.jpeg",
+  Setting: "France",
+  Type: "Hero",
+  Quote: "A dream is a wish your heart makes",
+  Nemesis: "Wicked Stepmother",
+  Love: "The Prince",
+  Sidekicks: "Fairy Godmother"
+}
+
+Alice in Wonderland = {
+  Name: "Alice",
+  Film: "Alice in Wonderland",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-agg0k5_1282af1a.jpeg?region=0,0,400,225",
+  Setting: "Wonderland",
+  Type: "Hero",
+  Quote: "Curiouser and curiouser",
+  Nemesis: "Queen of Hearts",
+  Love: "None",
+  Sidekicks: "Cheshire Cat"
+}
+
+Sleeping Beauty = {
+  Name: "Aurora",
+  Film: "Sleeping Beauty",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/character_princess_aurora_52a72304.jpeg?region=0%2C0%2C450%2C450",
+  Setting: "Germany",
+  Type: "Hero",
+  Quote: "I walked with you once upon a dream",
+  Nemesis: "Maleficent",
+  Love: "Prince Phillip",
+  Sidekicks: "Flora, Fauna, and Merryweather"
+}
+
+The Little Mermaid = {
+  Name: "Ariel",
+  Film: "The Little Mermaid",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/0cb87bf0c4c99068e6c0160813500b94a53dd05b.jpeg?region=0%2C0%2C600%2C600&amp;width=160",
+  Setting: "Denmark, Under the Sea",
+  Type: "Hero",
+  Quote: "Wish I could be part of your world",
+  Nemesis: "Ursula",
+  Love: "Prince Eric",
+  Sidekicks: "Flounder, Sebastian"
+}
+
+Beauty and the Beast = {
+  Name: "Belle",
+  Film: "Beauty and the Beast",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/character_princess_belle_c12c4bf1.jpeg?region=0,0,450,450",
+  Setting: "France",
+  Type: "Hero",
+  Quote: "I want much more than this provincial life",
+  Nemesis: "Gaston",
+  Love: "The Beast",
+  Sidekicks: "Lumierie, Cogsworth, Mrs. Potts"
+}
+
+Aladdin = [ {
+  Name: "Aladdin",
+  Film: "Aladdin",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/character_aladdin_aladdin_d8638d36.jpeg",
+  Setting: "Agrabah",
+  Type: "Hero",
+  Quote: "Gotta Keep One Jump Ahead",
+  Nemesis: "Jafar",
+  Love: "Jasmine",
+  Sidekicks: "Abu"
+}, {
+  Name: "Jasmine",
+  Film: "Aladdin",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/image_0c460f6e.jpeg",
+  Setting: "Agrabah",
+  Type: "Love Interest",
+  Quote: "Now I'm in a whole new world with you",
+  Nemesis: "Jafar",
+  Love: "Aladdin",
+  Sidekicks: "Rajah"
+}, {
+  Name: "Jafar",
+  Film: "Aladdin",
+  Photo: "http://s9.postimg.org/m6eszerv3/Jafar.jpg",
+  Setting: "Agrabah",
+  Type: "Villain",
+  Quote: "You will order the princess to marry me",
+  Nemesis: "Aladdin",
+  Love: "None",
+  Sidekicks: "Iago"
+}, {
+  Name: "Genie",
+  Film: "Aladdin",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/image_8ab081a8.jpeg",
+  Setting: "Agrabah",
+  Type: "Sidekick",
+  Quote: "You ain't never had a friend like me",
+  Nemesis: "Jafar",
+  Love: "None",
+  Sidekicks: "Magic Carpet"
+}, {
+  Name: "Abu",
+  Film: "Aladdin",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/character_aladdin_abu_bb8ccab2.jpeg",
+  Setting: "Agrabah",
+  Type: "Sidekick",
+  Quote: "",
+  Nemesis: "Jafar",
+  Love: "None",
+  Sidekicks: "Magic Carpet"
+}, {
+  Name: "Iago",
+  Film: "Aladdin",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/image_c458dd82.jpeg",
+  Setting: "Agrabah",
+  Type: "Sidekick",
+  Quote: "Yes, on mighty evil one",
+  Nemesis: "Aladdin",
+  Love: "None",
+  Sidekicks: ""
+} ]
+
+The Lion King = [{
+  Name: "Simba",
+  Film: "The Lion King",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/character_thelionking_simba_a4161276.jpeg?region=0%2C0%2C450%2C450&amp;width=160",
+  Setting: "Africa",
+  Type: "Hero",
+  Quote: "I just can't wait to be king",
+  Nemesis: "Aladdin",
+  Love: "Nala",
+  Sidekicks: "Timon, Pumba"
+}, {
+  Name: "Nala",
+  Film: "The Lion King",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/character_thelionking_nala_7b0cb360.jpeg?region=0%2C0%2C450%2C450&amp;width=160",
+  Setting: "Africa",
+  Type: "Love Interest",
+  Quote: "Can you feel the love tonight?",
+  Nemesis: "Aladdin",
+  Love: "Simba",
+  Sidekicks: ""
+}]
+
+Pocahontas = {
+  Name: "Pocahontas",
+  Film: "Pocahontas",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/character_princess_pocahontas_a3f01a3a.jpeg?region=0,0,450,450",
+  Setting: "Jamestown, Virginia",
+  Type: "Hero",
+  Quote: "You can paint with all the colors of the wind",
+  Nemesis: "Governor Ratcliffe",
+  Love: "John Smith",
+  Sidekicks: "Meeko"
+}
+
+The Hunchback of Notre Dame = {
+  Name: "Quasimodo",
+  Film: "The Hunchback of Notre Dame",
+  Photo: "https://a.dilcdn.com/bl/wp-content/uploads/sites/25/2013/03/90s-Hunchback-of-Notre-Dame.png",
+  Setting: "France",
+  Type: "Hero",
+  Quote: "I'll have spent one day out there",
+  Nemesis: "Monseigneur Claude Frollo",
+  Love: "Esmerelda",
+  Sidekicks: "Gargoyles"
+}
+
+Hercules = {
+  Name: "Hercules",
+  Film: "Hercules",
+  Photo: "http://orig06.deviantart.net/343f/f/2015/193/0/8/disney_hercules_x_reader_by_scrougeofares-d91203m.jpg",
+  Setting: "Greece",
+  Type: "Hero",
+  Quote: "Go the distance",
+  Nemesis: "Hades",
+  Love: "Megara",
+  Sidekicks: "Philoctetes"
+}
+
+Mulan = {
+  Name: "Mulan",
+  Film: "Mulan",
+  Photo: "http://www.wdwinfo.com/wp-content/uploads/2016/10/mulan.jpg",
+  Setting: "China",
+  Type: "Hero",
+  Quote: "When will my reflection show who I am inside?",
+  Nemesis: "Shan Yu",
+  Love: "Li Shang",
+  Sidekicks: "Mushu"
+}
+
+Tarzan = {
+  Name: "Tarzan",
+  Film: "Tarzan",
+  Photo: "https://s-media-cache-ak0.pinimg.com/originals/78/8e/4d/788e4db77c85fd0c3d34fc0f409bb069.jpg",
+  Setting: "Africa",
+  Type: "Hero",
+  Quote: "",
+  Nemesis: "Clayton",
+  Love: "Jane Porter",
+  Sidekicks: "Terk, Tantor"
+}
+
+Lilo & Stitch = [{
+  Name: "Stitch",
+  Film: "Lilo & Stitch",
+  Photo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Stitch_%28Lilo_%26_Stitch%29.svg/1200px-Stitch_%28Lilo_%26_Stitch%29.svg.png",
+  Setting: "Hawaii, Outer space",
+  Type: "Hero",
+  Quote: "",
+  Nemesis: "Dr. Jumba Jookiba",
+  Love: "Elvis",
+  Sidekicks: "Lilo"
+}, {
+  Name: "Lilo",
+  Film: "Lilo & Stitch",
+  Photo: "http://vignette2.wikia.nocookie.net/lilostitchtheseries/images/5/57/Lilo-Pelekai6.jpg/revision/latest?cb=20110904054538",
+  Setting: "Hawaii",
+  Type: "Hero",
+  Quote: "'Ohana' means 'family.' 'Family' means 'no one gets left behind.'",
+  Nemesis: "Cobra Bubbles",
+  Love: "Elvis",
+  Sidekicks: "Stitch"
+}]
+
+Finding Nemo = [{
+  Name: "Nemo",
+  Film: "Finding Nemo",
+  Photo: "http://vignette4.wikia.nocookie.net/pixar/images/a/aa/Nemo-FN.png/revision/latest?cb=20160710221104",
+  Setting: "Great Barrier Reef",
+  Type: "Hero",
+  Quote: "",
+  Nemesis: "Sharks",
+  Love: "",
+  Sidekicks: ""
+}, {
+  Name: "Dory",
+  Film: "Finding Nemo",
+  Photo: "http://vignette3.wikia.nocookie.net/pixar/images/a/af/Dory_FD.jpg/revision/latest?cb=20160330204558",
+  Setting: "Great Barrier Reef",
+  Type: "Sidekick",
+  Quote: "Just keep swimming",
+  Nemesis: "Memory Loss",
+  Love: "",
+  Sidekicks: "Marlin"
+}]
+
+The Princess and the Frog = {
+  Name: "Tiana",
+  Film: "The Princess and the Frog",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/character_princess_tiana_ed266702.jpeg?region=0%2C0%2C450%2C450",
+  Setting: "New Orleans",
+  Type: "Hero",
+  Quote: "",
+  Nemesis: "Dr. Facilier",
+  Love: "Prince Naveen",
+  Sidekicks: "Frog Prince Naveen"
+}
+
+Brave = {
+  Name: "Merida",
+  Film: "Brave",
+  Photo: "http://disneyexaminer.com/wp-content/uploads/2015/10/disney-movie-quotes-merida.jpg",
+  Setting: "Scottland",
+  Type: "Hero",
+  Quote: "",
+  Nemesis: "",
+  Love: "",
+  Sidekicks: "Angus"
+}
+
+//-----------------
+//GA PRESENTATION
+//-----------------
+Frozen = [{
+  Name: "Elsa",
+  Film: "Frozen",
+  Photo: "https://lumiere-a.akamaihd.net/v1/images/6d7454cea6644379adc7e529c5790a28078a2823.jpeg?region=0,0,450,450",
+  Setting: "Arendelle",
+  Type: "Hero",
+  Quote: "Let it go",
+  Nemesis: "",
+  Love: "",
+  Sidekicks: ""
+}, {
+  Name: "Anna",
+  Film: "Frozen",
+  Photo: "http://www.clipartkid.com/images/76/anna-frozen-photo-35904954-fanpop-OhtDDD-clipart.png",
+  Setting: "Arendelle",
+  Type: "Hero",
+  Quote: "Do you want to build a snowman?",
+  Nemesis: "Hans",
+  Love: "Kristoff",
+  Sidekicks: "Olaf"
+}]
---
+++ data/films.js
@@ -0,0 +1,45 @@
+//LIST OF FILMS FOR HEROKU ADD THROUGH API
+//NOT A DATA SEED
+//SOURCE: https://en.wikipedia.org/wiki/List_of_Disney_theatrical_animated_features
+
+//SORTED BY RELEASE DATE, PARTIAL LIST
+
+// Snow White and the Seven Dwarfs
+// Pinocchio
+// Dumbo
+// Bambi
+// Cinderella
+// Alice in Wonderland
+// Peter Pan
+// Lady and the Tramp
+// Sleeping Beauty
+// One Hundred and One Dalmatians
+// The Sword in the Stone
+// The Jungle Book
+// Robin Hood
+// The Many Adventures of Winnie the Pooh
+// The Little Mermaid
+// Beauty and the Beast
+// Aladdin
+// The Lion King
+// Pocahontas
+// The Hunchback of Notre Dame
+// Hercules
+// Mulan
+// Tarzan
+// Atlantis: The Lost Empire
+// Lilo & Stitch
+// Finding Nemo
+// WALL-E
+// Up
+// The Princess and the Frog
+// Brave
+// Inside Out
+// Moana
+
+
+//-----------------
+//GA PRESENTATION
+//-----------------
+
+//Frozen
